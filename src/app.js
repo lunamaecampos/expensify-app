0b4487a48c044fc3f47ae2400895fdab8a30d3ef
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2018-09-27 00:34:30 -0700

    redux with moment js and react-dates

--- src/app.js
+++ src/app.js
@@ -11,16 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}));
-store.dispatch(addExpense({ description: 'Gas Bill'}));
-
-store.dispatch(setTextFilter('water'));
-
-
-setTimeout(()=> {
-  store.dispatch(setTextFilter('bill'));
-}, 3000)
-
+store.dispatch(addExpense({ description: 'Rent', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 1009500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
